6feeaf11b650c9a6f6c922a2aa974746e17db612
J Maine <33839097+xjmaine@users.noreply.github.com>
2022-12-18 20:39:44 +0000

    Update ChatClient.java

--- ChatClient.java
+++ ChatClient.java
@@ -1,15 +1,26 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 public class ChatClient {
-  public static void main(String[] args) {
-    try {
-      // Connect to the server
-      Socket socket = new Socket("localhost", 9999);
-      System.out.println("Connected to the server: " + socket);
+  public static void main(String[] args) throws Exception {
+    // Create a client socket
+    Socket socket = new Socket("localhost", 6789);
+    System.out.println("Connected to chat server");
 
-      // Set up input and output streams for the client
-      BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-      BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System
+    // Create input and output streams for the socket
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+    // Read messages from the user and send them to the server
+    BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+    while (true) {
+      String message = userInput.readLine();
+      out.println(message);
+      System.out.println("Sent: " + message);
+
+      // Read and display the server's response
+      String response = in.readLine();
+      System.out.println("Received: " + response);
+    }
+  }
+}
